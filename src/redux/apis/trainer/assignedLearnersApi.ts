04e7dab6ad71b6d4c0a241bc9fbb444c4049abcc
aothymoon59 <aothymoon59@gmail.com>
2026-04-01 01:32:24 +0600

    :sparkles: assigner learner get done

--- src/redux/apis/trainer/assignedLearnersApi.ts
+++ src/redux/apis/trainer/assignedLearnersApi.ts
@@ -1,9 +1,47 @@
 import { api } from "../../api";
-import type { EnrolmentListResponse } from "../enrolmentApi";
+
+export interface TrainerAssignedEnrolment {
+  id: string;
+  enrolment_number: string;
+  status: string;
+  payment_status: string;
+  enrolled_at: string;
+  access_expires_at: string | null;
+  completed_at: string | null;
+  learner: {
+    id: string;
+    name: string;
+    email: string;
+  };
+  qualification: {
+    id: string;
+    title: string;
+    slug: string;
+    is_cpd: boolean;
+  };
+  trainer: {
+    id: string;
+    name: string;
+    email: string;
+    role: string;
+  };
+  iqa: {
+    id: string;
+    name: string;
+    email: string;
+    role: string;
+  } | null;
+}
+
+export interface TrainerAssignedEnrolmentsResponse {
+  success: boolean;
+  message: string;
+  data: TrainerAssignedEnrolment[];
+}
 
 export const assignedLearnersApi = api.injectEndpoints({
   endpoints: (builder) => ({
-    getTrainerEnrolments: builder.query<EnrolmentListResponse, void>({
+    getTrainerEnrolments: builder.query<TrainerAssignedEnrolmentsResponse, void>({
       query: () => ({
         url: "/api/enrolments/trainer/my-enrolments/",
         method: "GET",
